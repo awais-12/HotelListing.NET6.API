8fb41127cc23f2d4992a9f8e9607650874d90a4d
apple <awaisiftikhar480@gmail.com>
2025-05-15 00:52:08 +0500

    feat/ProtectAPI-RefreshToken
    ProtectAPI
    RefreshToken Implementation

--- HotelListing.NET6/Controllers/AccountController.cs
+++ HotelListing.NET6/Controllers/AccountController.cs
@@ -59,5 +59,22 @@ namespace HotelListing.NET6.Controllers
 
             return Ok(authResponse);
         }
+
+
+        [HttpPost]
+        [Route("refreshToken")]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public async Task<ActionResult> RefreshToken([FromBody] AuthResponseDto request)
+        {
+            var authResponse = await _manager.VerifyRefreshToken(request);
+            if (authResponse == null)
+            {
+                return Unauthorized();
+            }
+
+            return Ok(authResponse);
+        }
     }
 }
