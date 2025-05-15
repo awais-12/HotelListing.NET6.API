6cd7583fa4208fa3d7ef6e97e6e35d02d28c1afd
apple <awaisiftikhar480@gmail.com>
2025-05-16 04:03:36 +0500

    feat/FactoryDbContext
    DBContext New Class Add Factory

--- HotelListing.API.Data/HotelListingDbContext.cs
+++ HotelListing.API.Data/HotelListingDbContext.cs
@@ -2,6 +2,8 @@
 using HotelListing.API.Data.Configurations;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
 
 namespace HotelListing.API.Data
 {
@@ -75,6 +77,22 @@ namespace HotelListing.API.Data
         //        });
         //}
     }
+    public class HotelListingDbContextFactory : IDesignTimeDbContextFactory<HotelListingDbContext>
+    {
+        public HotelListingDbContext CreateDbContext(string[] args)
+        {
+            IConfiguration config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .Build();
 
+            var optionsBuilder = new DbContextOptionsBuilder<HotelListingDbContext>();
+            var conn = config.GetConnectionString("HotelListingDbConnection");///ConnectionString
+
+            optionsBuilder.UseNpgsql(conn); // PostgreSQL here
+
+            return new HotelListingDbContext(optionsBuilder.Options);
+        }
+    }
 }
 
