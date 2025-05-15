85e7cd9eddba2c93b8777eb289faac15fde06879
apple <awaisiftikhar480@gmail.com>
2025-05-15 19:28:47 +0500

    feat/ImplementPagination

--- HotelListing.NET6/Contracts/IGenericRepository.cs
+++ HotelListing.NET6/Contracts/IGenericRepository.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Linq.Expressions;
+using HotelListing.NET6.Models;
 
 namespace HotelListing.NET6.Contracts
 {
     public interface IGenericRepository<T> where T : class
     {
         Task<List<T>> GetAllAsync();
+        Task<PagedResult<TResult>> GetAllAsync<TResult>(QueryParameters queryParameter);
         Task<T> AddAsync(T entity);
         Task<T> GetAsync(int id);
         Task DeleteAsync(int id);
--- HotelListing.NET6/Repository/GenericRepository.cs
+++ HotelListing.NET6/Repository/GenericRepository.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Linq.Expressions;
+using AutoMapper;
+using AutoMapper.QueryableExtensions;
 using HotelListing.NET6.Contracts;
 using HotelListing.NET6.Data;
+using HotelListing.NET6.Models;
 using Microsoft.EntityFrameworkCore;
 
 namespace HotelListing.NET6.Repository
@@ -9,10 +12,12 @@ namespace HotelListing.NET6.Repository
     public class GenericRepository<T> : IGenericRepository<T> where T : class
     {
         private readonly HotelListingDbContext _context;
+        private readonly IMapper _mapper;
 
-        public GenericRepository(HotelListingDbContext context)
+        public GenericRepository(HotelListingDbContext context,IMapper mapper)
         {
             _context = context;
+            _mapper = mapper;
         }
 
         public async Task<T> AddAsync(T entity)
@@ -62,6 +67,24 @@ namespace HotelListing.NET6.Repository
         public IQueryable<T> FindByConditionWithTracking(Expression<Func<T, bool>> expression)
         {
             return _context.Set<T>().Where(expression);
+        }
+
+        public async Task<PagedResult<TResult>> GetAllAsync<TResult>(QueryParameters queryParameter)
+        {
+            var totalSize = await _context.Set<T>().CountAsync();
+            var items = await _context.Set<T>()
+                .Skip(queryParameter.StartIndex)
+                .Take(queryParameter.PageSize)
+                .ProjectTo<TResult>(_mapper.ConfigurationProvider)
+                .ToListAsync();
+
+            return new PagedResult<TResult>()
+            {
+                TotalCount = totalSize,
+                Items = items,
+                PageNumber = queryParameter.PageNumber,
+                RecordNumber = queryParameter.PageSize
+            };
         }
     }
 }
